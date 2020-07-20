bc4ec410faf9c53d1f09c71e74511d76f7481e1c
nicaberry <nicaberry@yandex.ru>
2020-07-20 17:30:24 +0300

    newdzn

--- script/main.js
+++ script/main.js
@@ -1,15 +1,21 @@
 window.onload = (e) => {
     let divAboutMe = document.querySelector(".about-me");
-    let btnOpenAboutMe = document.querySelector("#about-me__buttonOpen");
+    let btnOpenAboutMeMax = document.querySelector("#about-me__buttonOpen_max");
+    let btnOpenAboutMeMin = document.querySelector("#about-me__buttonOpen_min");
     let btnCloseAboutMe = document.querySelector("#about-me__buttonClose");
 
+
+    btnOpenAboutMeMin.onclick = (e) => {
+        divAboutMe.style.display = "grid";
+    }
+
+    btnOpenAboutMeMax.onclick = (e) => {
+        divAboutMe.style.display = "grid";
+    }
 
     btnCloseAboutMe.onclick = (e) => {
         divAboutMe.style.display = "none";
     }
 
-    btnOpenAboutMe.onclick = (e) => {
-        divAboutMe.style.display = "grid";
-    }
     
 }
