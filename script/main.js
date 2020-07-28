635c75b6a0fb228a69c6d61171f3e8b1b215e769
nicaberry <nicaberry@yandex.ru>
2020-07-28 08:17:47 +0300

    autoCards

--- script/main.js
+++ script/main.js
@@ -1,4 +1,153 @@
 window.onload = (e) => {
+    
+        //  CARDS
+        let infoAboutMyWorkForCard = [
+            {
+                name: "Itten Circle",
+                subname: "JAVASCRIPT/CSS/HTML",
+                href: "./myWork/IttienCircle/index.html",
+                githubhref: "https://github.com/nicaberry/IttenCircle",
+                image: "./images/myWork/IttenCircle.png",
+                imageAlt: "Itten Circle",
+                description: "Выпускной проект IT Academy."
+            },
+            {
+                name: "GameXO",
+                subname: "JAVASCRIPT/CSS/HTML",
+                href: "./myWork/GameXO/index.html",
+                githubhref: "https://github.com/nicaberry/GameXO",
+                image: "./images/myWork/GameXO.png",
+                imageAlt: "game XO",
+                description: "Игра 'крестики и нолики'."
+            },
+            {
+                name: "Calculator",
+                subname: "JAVASCRIPT/CSS/HTML",
+                href: "./myWork/Calculator/index.html",
+                githubhref: "https://github.com/nicaberry/calculator",
+                image: "./images/myWork/Calculator.png",
+                imageAlt: "calculator",
+                description: "Простой калькулятор на JavaScript. Выполняет простейшие арифметические операции."
+            },
+            {
+                name: "Game Life", 
+                subname: "JAVASCRIPT/HTML",
+                href: "./myWork/GameLive/index.html",
+                githubhref: "https://github.com/nicaberry/game-live",
+                image: "./images/myWork/GameLife.png",
+                imageAlt: "game life",
+                description: "Игра 'жизнь'."
+            },
+            {
+                name: "Barley-break",
+                subname: "JAVASCRIPT/CSS/HTML",
+                href: "./myWork/BarleyBreak/index.html",
+                githubhref: "https://github.com/nicaberry/game-15",
+                image: "./images/myWork/BarleyBreak.png",
+                imageAlt: "barley-break",
+                description: "Игра 'пятнашки'"
+            },
+            {
+                name: "Snake Todo",
+                subname: "JAVASCRIPT/HTML",
+                href: "./myWork/SnakeTodo/index.html",
+                githubhref: "https://github.com/nicaberry/game-15",
+                image: "./images/myWork/SnakeTodo.png",
+                imageAlt: "game Snake",
+                description: "Игра 'змейка'."
+            },
+            {
+                name: "Task",
+                subname: "JAVASCRIPT/CSS/HTML",
+                href: "./myWork/Task/index.html",
+                githubhref: "https://github.com/nicaberry/task",
+                image: "./images/myWork/Task.png",
+                imageAlt: "todo list",
+                description: "Приложение 'список дел'."
+            },
+        ];
+
+        // CREATE CARDS 
+
+        function createCards(info) {
+            let cards = [];
+            info.forEach((item) => {
+                // card
+                let card = document.createElement("div");
+                card.classList.add("card");
+                card.setAttribute("data-active", "true");
+
+                //card-img
+                let cardImg = document.createElement("div");
+                cardImg.classList.add("card-img");
+
+                let buttonOpenDescription = document.createElement("button");
+                buttonOpenDescription.classList.add("card-description__button", "card-description__button_open");
+                buttonOpenDescription.innerHTML = "&#8801;";
+
+                let buttonCloseDescription = document.createElement("button");
+                buttonCloseDescription.classList.add("card-description__button", "card-description__button_close");
+                buttonCloseDescription.innerHTML = "&#215;";
+
+                let cardDescription = document.createElement("p");
+                cardDescription.classList.add("card-description");
+                cardDescription.innerHTML = item.description;
+
+                let image = document.createElement("img");
+                image.setAttribute("src", item.image);
+                image.setAttribute("alt", item.imageAlt);
+
+                cardImg.append(buttonOpenDescription);
+                cardImg.append(buttonCloseDescription);
+                cardImg.append(cardDescription);
+                cardImg.append(image);
+
+                //card-header
+                let cardHeader = document.createElement("div");
+                cardHeader.classList.add("card-header");
+
+                let cardName = document.createElement("a");
+                cardName.classList.add("card-header__name");
+                cardName.setAttribute("href", item.href);
+                cardName.setAttribute("target", "_blank");
+                cardName.innerHTML = item.name;
+
+                let cardSubname = document.createElement("p");
+                cardSubname.classList.add("card-header__subname");
+                cardSubname.innerHTML = item.subname;
+
+                let cardGithub = document.createElement("a");
+                cardGithub.classList.add("card-header__github");
+                cardGithub.setAttribute("href", item.githubhref);
+                cardGithub.setAttribute("target", "_blank");
+                cardGithub.innerHTML = "GITHUB";
+
+                cardHeader.append(cardName);
+                cardHeader.append(cardSubname);
+                cardHeader.append(cardGithub);
+
+                card.append(cardImg);
+                card.append(cardHeader);
+
+                cards.push(card);
+            })
+
+            return cards;
+        }
+
+        let cardsArray = createCards(infoAboutMyWorkForCard);
+
+        function showMyCards(cards) {
+            let myWorkContainer = document.querySelector(".my-work-container");
+
+            cards.forEach((item) => {
+                myWorkContainer.append(item);
+            });
+        }
+
+        showMyCards(cardsArray);
+
+        
     // ABOUT-ME
     let divAboutMe = document.querySelector(".about-me");
     let btnOpenAboutMeMax = document.querySelector("#about-me__buttonOpen_max");
@@ -24,22 +173,22 @@ window.onload = (e) => {
 
     for (let i = 0; i < cards.length; i++) {
         cards[i].onclick = (e) => {
-            if (e.target.classList.contains("card-header__descriptionOpenButton")) {
+            if (e.target.classList.contains("card-description__button_open")) {
                 e.target.style.display = "none";
 
-                let buttonClose = e.target.parentNode.querySelector(".card-header__descriptionCloseButton");
+                let buttonClose = e.target.parentNode.querySelector(".card-description__button_close");
                 buttonClose.style.display = "block";
 
-                let description = e.target.parentNode.querySelector(".card-header__description");
+                let description = e.target.parentNode.querySelector(".card-description");
                 description.style.display = "block";
             }
-            if (e.target.classList.contains("card-header__descriptionCloseButton")) {
+            if (e.target.classList.contains("card-description__button_close")) {
                 e.target.style.display = "none";
 
-                let buttonOpen = e.target.parentNode.querySelector(".card-header__descriptionOpenButton");
+                let buttonOpen = e.target.parentNode.querySelector(".card-description__button_open");
                 buttonOpen.style.display = "block";
 
-                let description = e.target.parentNode.querySelector(".card-header__description");
+                let description = e.target.parentNode.querySelector(".card-description");
                 description.style.display = "none";
             }
         }
@@ -164,6 +313,7 @@ window.onload = (e) => {
             }
         }
         
+
     }
     
     
